20ba63ce982ff4aa70e8ef39f7d9877cc38e16cf
Luke Bullard <lukeb@agnesanddora.com>
2018-06-12 13:23:39 -0600

    initial commit

---
+++ aws.mod.php
@@ -0,0 +1,59 @@
+<?php
+    /**
+     * AWS Module for OliveWeb
+     * Luke Bullard, May 2018
+     */
+    
+    //make sure we are included securely
+    if (!defined("INPROCESS")) { header("HTTP/1.0 403 Forbidden"); exit(0); }
+
+    use Aws\Common\Aws;
+
+    /**
+     * The AWS OliveWeb Module
+     */
+    class MOD_aws
+    {
+        private $m_dynamoDB;
+        private $m_aws;
+
+        public function __construct()
+        {
+            require_once("vendor/autoload.php");
+        }
+
+        /**
+         * Retrieves the AWS object. Creates the AWS object if it has not been created already.
+         * 
+         * @return Aws
+         */
+        public function getAWS()
+        {
+            if (isset($this->m_aws))
+            {
+                return $this->m_aws;
+            }
+
+            $this->m_aws = Aws::factory(__DIR__ . "/config.php");
+            return $this->m_aws;
+        }
+
+        /**
+         * Retrieves the DynamoDB object from AWS.
+         * 
+         * @return DynamoDbClient
+         */
+        public function getDynamoDB()
+        {
+            if (isset($this->m_dynamoDB))
+            {
+                return $this->m_dynamoDB;
+            }
+
+            $aws = $this->getAWS();
+            $this->m_dynamoDB = $aws->get("DynamoDb");
+
+            return $this->m_dynamoDB;
+        }
+    }
+?>
